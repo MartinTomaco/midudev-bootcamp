ca5fb564d9973573c4960c1b84cd3ab7e760cda7
Martín González <marttingonzalez@gmail.com>
2021-02-01 09:54:53 -0300

    primer commit

---
+++ part1-react/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import Mensaje from './Mensaje.js'
+
+const Description  = () => {
+  return <p> Esta es la app del curso fullstack bootcamp</p>
+}
+
+function App() {
+
+  return (
+    <div className="App">
+     <Mensaje color='red' message='Estamos trabajando'/>
+     <Mensaje color='yellow' message='en un curso'/>
+     <Mensaje color='green' message='de react'/>
+     <Description/>
+    </div>
+  );
+}
+
+export default App;
